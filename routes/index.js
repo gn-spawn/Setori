c032faa6e887ad08ecbe643d68a2ebb5fc3a0c17
Spawn <spawn@SpawnnoMacBook.local>
2018-03-14 14:27:40 +0900

    multer入れた

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import multer from 'multer';
 const router = express.Router();
+const upload = multer({ inMemory: true })
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/upload', upload.single('playlist') ,(req, res) => {
+  res.send(req.file.originalname)
+})
 
 module.exports = router;
