e2571609d412615999eb111ca0838f7d9edde03c
Spawn <spawn@SpawnnoMacBook.local>
2018-03-14 15:27:19 +0900

    大まかにできた

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,42 @@
 import express from 'express';
 import multer from 'multer';
+import plist from 'plist';
+import fs from 'fs';
+
 const router = express.Router();
-const upload = multer({ inMemory: true })
+const storage = multer.memoryStorage()
+
+const upload = multer({ dest: 'uploads' })
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/upload', upload.single('playlist') ,(req, res) => {
-  res.send(req.file.originalname)
+router.post('/upload', upload.single('playlist'), (req, res) => {
+  const playlist = []
+  const xml = plist.parse(fs.readFileSync(req.file.path, 'utf8'))
+  get_playlist(xml).forEach(song => {
+    // playlist.push(`${song['Name']} / ${song['Artist']}`)
+    playlist.push({
+      'song': song['Name'],
+      'artist': song['Artist'],
+    })
+  })
+
+  res.render('playlist', { playlist })
+  fs.unlink(req.file.path)
 })
+
+function get_playlist(xml) {
+  const tracks = xml.Tracks
+  const playlist = xml.Playlists[0]['Playlist Items']
+  const setori = []
+  playlist.forEach(track => {
+    const id = track['Track ID']
+    setori.push(tracks[id])
+  });
+  return setori
+}
 
 module.exports = router;
